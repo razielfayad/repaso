22a413bed739e09baab1e8129fab316106ecfaff
Juan C. Perez <22458962+razielfayad@users.noreply.github.com>
2022-08-13 10:14:44 -0500

    nueva línea de código para rama ModalidadMovil

--- src/main/java/com/ciclo3/inicio/Inicio.java
+++ src/main/java/com/ciclo3/inicio/Inicio.java
@@ -4,5 +4,6 @@ public class Inicio {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("nueva línea de código");
+        System.out.println("línea que solo está en la rama ModalidadMovil");
     }
 }
