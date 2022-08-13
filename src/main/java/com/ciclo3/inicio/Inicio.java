4543f3cf3f9bbed38f43f5a7578a4e9ed25b9d48
Juan C. Perez <22458962+razielfayad@users.noreply.github.com>
2022-08-13 09:44:24 -0500

    nueva línea de código

--- src/main/java/com/ciclo3/inicio/Inicio.java
+++ src/main/java/com/ciclo3/inicio/Inicio.java
@@ -3,5 +3,6 @@ package com.ciclo3.inicio;
 public class Inicio {
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("nueva línea de código");
     }
 }
